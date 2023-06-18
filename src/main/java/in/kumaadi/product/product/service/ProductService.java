ec3f712560f3d292cc02343359b0a9f49fefba49
Aditya Kumar <adi341875@gmail.com>
2023-06-19 01:41:57 +0530

    Creating dummy products in PRODUCT tablle

--- src/main/java/in/kumaadi/product/product/service/ProductService.java
+++ src/main/java/in/kumaadi/product/product/service/ProductService.java
@@ -1,5 +1,10 @@
 package in.kumaadi.product.product.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +17,33 @@ public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
-    public Product getProduct(int id) {
-        return productRepository.findById(id).get();
+
+    // public Product getProduct(int id) {
+       
+    // }
+
+    final static Logger Log = LoggerFactory.getLogger(ProductService.class);
+    public List<Product> getProducts(){
+        createDummyProduct();
+        Log.info("Dummy Prod Creaated");
+        return productRepository.findAll();
     }
+
+
+    public void createDummyProduct()
+	{
+		List<Product> list = new ArrayList<>();
+        list.add(new Product(1,"RED","SHORT",'M'));
+        
+        list.add(new Product(2,"RED","SHORT",'M'));
+        
+        list.add(new Product(3,"RED","SHORT",'M'));
+        
+        list.add(new Product(4,"RED","SHORT",'M'));
+        
+        list.add(new Product(5,"RED","SHORT",'M'));
+        productRepository.saveAll(list);
+	}
     
 
 }
